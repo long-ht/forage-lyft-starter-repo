d0531108e169bfb92c8ccf9c58f9776521dcf39b
Long Tran <long-ht@outlook.com>
2022-09-08 18:38:14 -0600

    task 2

---
+++ src/car.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+
+class Serviceable(ABC):
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() | self.battery.needs_service()
